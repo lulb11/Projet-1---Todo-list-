c6979877be9972849474d48697de8a9782f84032
Nelia <nelia.courtais@live.fr>
2023-10-02 20:09:17 +0200

    nothing

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const categoryHome = document.querySelector('.section-category-home');
-const categoryWork = document.querySelector('.section-category-work');
-const categoryChill = document.querySelector('.section-category-chill');
-const myForm = document.querySelector('.myForm');
+const categoryHome = document.querySelector(".section-category-home");
+const categoryWork = document.querySelector(".section-category-work");
+const categoryChill = document.querySelector(".section-category-chill");
+const myForm = document.querySelector(".myForm");
 
-
-myForm.addEventListener('submit', (event) => {
+myForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const newTodo = {
     category: event.target.elements[0].value, // home work chill
     description: event.target.elements[1].value, // text area
-    status: "ToDo" // a adapter selon la gestion du status (Je n'ai malheureusement pas compris cette partie)
+    status: "ToDo", // a adapter selon la gestion du status (Je n'ai malheureusement pas compris cette partie)
   };
-  
-const content = document.createElement('div');
-content.className = 'section-task';
-content.innerHTML = `
+
+  const content = document.createElement("div");
+  content.className = "section-task";
+  content.innerHTML = `
 <div class="merge-delete-text">
   <div class="delete-button">
     <div class="delete-button-content">-</div>
@@ -29,30 +28,26 @@ content.innerHTML = `
 </div>
 </div>`;
 
-if (newTodo.category === "home") {
-  categoryHome.appendChild(content);
-}
-else if (newTodo.category === "work") {
-  categoryWork.appendChild(content);
-}
-else {
-  categoryChill.appendChild(content);
-}
-event.target.elements[1].value = "";
+  if (newTodo.category === "home") {
+    categoryHome.appendChild(content);
+  } else if (newTodo.category === "work") {
+    categoryWork.appendChild(content);
+  } else {
+    categoryChill.appendChild(content);
+  }
+  event.target.elements[1].value = "";
 });
-
 
 //Pour le bouton de status :
 function myCallback() {
   const selectElement = document.getElementByClassName("status-button");
   // je vais chercher l'élement <select> avec sa class //
   const selectedOption = selectElement.options[selectElement.selectedIndex];
-  // je vais chercher l'option qui est sélectionnée en lui disant avec selectElement.selectedIndex 
+  // je vais chercher l'option qui est sélectionnée en lui disant avec selectElement.selectedIndex
   //que je récupère l'index de l'option puis j'accède à cet index avec "selectElement.options"//
   const selectedValue = selectedOption.value;
   // j'extrait la valeur de l'option sélectionnée. J'accède à la propriété "value" de mes options//
 
-  
   selectElement.className = selectedValue;
   // On applique la classe CSS correspondante à la valeur sélectionnée //
-};
+}
